7b6326c0a2bd0adb6c3770e7911cb261a689c2a2
kyle67 <daniels.kyle@gmail.com>
2020-01-22 22:42:13 -0500

    Modified GamePages

--- client/src/pages/GamePages/GamePages.js
+++ client/src/pages/GamePages/GamePages.js
@@ -1,201 +1,220 @@
-import React from "react";
+import React, {useState} from "react";
 import GameContainer from "./../components/GameComponents/GameContainer"
 import "./style.css"
+import ProfilePages from "../ProfilePages";
 
 
 
 
 function GamePages() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [currentAnswer, setAnswer] = useState('');
   const questions = [
     {
         "id": 1,
         "question": "What is a method?",
-        "correctAnswer": "A",
-        "randomAnswers": ["A,B,C,D"],
-        "language": "Javascript",
-        "difficulty": "Easy",
-        "points": 100
+        "level":"easy",
+        "answer_a":"11",
+        "answer_b": "34",
+        "answer_c": "33",
+        "answer_d": "33",
+        "points": "100"
     },
     {
-        "id": 2,
-        "question": "Question 2",
-        "correctAnswer": "C",
-        "randomAnswers": "A,B,C,D",
-        "language": "Node",
-        "difficulty": "Easy",
-        "points": 100
+      "id": 2,
+        "question": "What is a method?",
+        "level":"easy",
+        "answer_a":"11",
+        "answer_b": "34",
+        "answer_c": "33",
+        "answer_d": "33",
+        "points": "100"
+        
     },
     {
-        "id": 3,
-        "question": "Question 3",
-        "correctAnswer": "D",
-        "randomAnswers": "A,B,C,D",
-        "language": "Javascript",
-        "difficulty": "Easy",
-        "points": 100
+      "id": 3,
+      "question": "What is a method?",
+      "level":"easy",
+      "answer_a":"11",
+      "answer_b": "34",
+      "answer_c": "33",
+      "answer_d": "33",
+      "points": "100"
     },
     {
-        "id": 4,
-        "question": "Question 4",
-        "correctAnswer": "A",
-        "randomAnswers": "A,B,C,D",
-        "language": "HTML",
-        "difficulty": "Easy",
-        "points": 100
+      "id": 4,
+      "question": "What is a method?",
+      "level":"easy",
+      "answer_a":"11",
+      "answer_b": "34",
+      "answer_c": "33",
+      "answer_d": "33",
+      "points": "100"
     },
     {
-        "id": 5,
-        "question": "Question 5",
-        "correctAnswer": "D",
-        "randomAnswers": "A,B,C,D",
-        "language": "CSS",
-        "difficulty": "Easy",
-        "points": 100
+      "id": 5,
+      "question": "What is a method?",
+      "level":"easy",
+      "answer_a":"11",
+      "answer_b": "34",
+      "answer_c": "33",
+      "answer_d": "33",
+      "points": "100"
     },
     {
-        "id": 6,
-        "question": "Question 6",
-        "correctAnswer": "C",
-        "randomAnswers": "A,B,C,D",
-        "language": "Javascript",
-        "difficulty": "Medium",
-        "points": 200
+      "id": 6,
+      "question": "What is a method?",
+      "level":"easy",
+      "answer_a":"11",
+      "answer_b": "34",
+      "answer_c": "33",
+      "answer_d": "33",
+      "points": "100"
     },
     {
-        "id": 7,
-        "question": "Question 7",
-        "correctAnswer": "C",
-        "randomAnswers": "A,B,C,D",
-        "language": "CSS",
-        "difficulty": "Medium",
-        "points": 200
+      "id": 7,
+      "question": "What is a method?",
+      "level":"easy",
+      "answer_a":"11",
+      "answer_b": "34",
+      "answer_c": "33",
+      "answer_d": "33",
+      "points": "100"
     },
     {
-        "id": 8,
-        "question": "Question 8",
-        "correctAnswer": "A",
-        "randomAnswers": "A,B,C,D",
-        "language": "Javascript",
-        "difficulty": "Medium",
-        "points": 200
+      "id": 8,
+      "question": "What is a method?",
+      "level":"easy",
+      "answer_a":"11",
+      "answer_b": "34",
+      "answer_c": "33",
+      "answer_d": "33",
+      "points": "100"
     },
     {
-        "id": 9,
-        "question": "Question 9",
-        "correctAnswer": "A",
-        "randomAnswers": "A,B,C,D",
-        "language": "Javascript",
-        "difficulty": "Medium",
-        "points": 200
+      "id": 9,
+      "question": "What is a method?",
+      "level":"easy",
+      "answer_a":"11",
+      "answer_b": "34",
+      "answer_c": "33",
+      "answer_d": "33",
+      "points": "100"
     },
     {
-        "id": 10,
-        "question": "Question 10",
-        "correctAnswer": "A",
-        "randomAnswers": "A,B,C,D",
-        "language": "CSS",
-        "difficulty": "Easy",
-        "points": 100
+      "id": 10,
+        "question": "What is a method?",
+        "level":"easy",
+        "answer_a":"11",
+        "answer_b": "34",
+        "answer_c": "33",
+        "answer_d": "33",
+        "points": "100"
     },
     {
-        "id": 11,
-        "question": "Question 11",
-        "correctAnswer": "A",
-        "randomAnswers": "A,B,C,D",
-        "language": "Javascript",
-        "difficulty": "Hard",
-        "points": 300
+       "id": 11,
+        "question": "What is a method?",
+        "level":"easy",
+        "answer_a":"11",
+        "answer_b": "34",
+        "answer_c": "33",
+        "answer_d": "33",
+        "points": "100"
     },
     {
         "id": 12,
-        "question": "Question 12",
-        "correctAnswer": "D",
-        "randomAnswers": "A,B,C,D",
-        "language": "MySQL",
-        "difficulty": "Hard",
-        "points": 300
+        "question": "What is a method?",
+        "level":"easy",
+        "answer_a":"11",
+        "answer_b": "34",
+        "answer_c": "33",
+        "answer_d": "33",
+        "points": "100"
     },
     {
         "id": 13,
-        "question": "Question 13",
-        "correctAnswer": "B",
-        "randomAnswers": "A,B,C,D",
-        "language": "Javascript",
-        "difficulty": "Medium",
-        "points": 300
+        "question": "What is a method?",
+        "level":"easy",
+        "answer_a":"11",
+        "answer_b": "34",
+        "answer_c": "33",
+        "answer_d": "33",
+        "points": "100"
     },
     {
         "id": 14,
-        "question": "Question 14",
-        "correctAnswer": "D",
-        "randomAnswers": "A,B,C,D",
-        "language": "Javascript",
-        "difficulty": "Hard",
-        "points": 300
+        "question": "What is a method?",
+        "level":"easy",
+        "answer_a":"11",
+        "answer_b": "34",
+        "answer_c": "33",
+        "answer_d": "33",
+        "points": "100"
     },
     {
         "id": 15,
-        "question": "Question 15",
-        "correctAnswer": "A",
-        "randomAnswers": "A,B,C,D",
-        "language": "Javascript",
-        "difficulty": "Hard",
-        "points": 300
+        "question": "What is a method?",
+        "level":"easy",
+        "answer_a":"11",
+        "answer_b": "34",
+        "answer_c": "33",
+        "answer_d": "33",
+        "points": "100"
     },
     {
         "id": 16,
-        "question": "Question 16",
-        "correctAnswer": "A",
-        "randomAnswers": "A,B,C,D",
-        "language": "HTML",
-        "difficulty": "Hard",
-        "points": 300
+        "question": "What is a method?",
+        "level":"easy",
+        "answer_a":"11",
+        "answer_b": "34",
+        "answer_c": "33",
+        "answer_d": "33",
+        "points": "100"
     },
     {
         "id": 17,
-        "question": "Question 17",
-        "correctAnswer": "C",
-        "randomAnswers": "A,B,C,D",
-        "language": "Node",
-        "difficulty": "Medium",
-        "points": 200
+        "question": "What is a method?",
+        "level":"easy",
+        "answer_a":"11",
+        "answer_b": "34",
+        "answer_c": "33",
+        "answer_d": "33",
+        "points": "100"
     },
     {
         "id": 18,
-        "question": "Question 18",
-        "correctAnswer": "A",
-        "randomAnswers": "A,B,C,D",
-        "language": "Javascript",
-        "difficulty": "Easy",
-        "points": 100
+        "question": "What is a method?",
+        "level":"easy",
+        "answer_a":"11",
+        "answer_b": "34",
+        "answer_c": "33",
+        "answer_d": "33",
+        "points": "100"
     },
     {
         "id": 19,
-        "question": "Question 19",
-        "correctAnswer": "C",
-        "randomAnswers": "A,B,C,D",
-        "language": "Bootstrap",
-        "difficulty": "Medium",
-        "points": 100
+        "question": "What is a method?",
+        "level":"easy",
+        "answer_a":"11",
+        "answer_b": "34",
+        "answer_c": "33",
+        "answer_d": "33",
+        "points": "100"
     },
     {
         "id": 20,
-        "question": "Question 20",
-        "correctAnswer": "B",
-        "randomAnswers": "A,B,C,D",
-        "language": "Node",
-        "difficulty": "Hard",
-        "points": 300
+        
     }
 ];
 
-const question = questions[0];
+const question = questions[currentQuestion];
   return (
     <div className="container">
      
       <GameContainer  total ="20" current="1"
                       question = {question.question}
-                      letter="A" answer="Hyper Text Markup Language"
+                      letter="a" answer={question.answer_a}
+                     
                        />
 
     </div>
