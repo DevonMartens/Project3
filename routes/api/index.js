34ddcdcc228ea4517f14da78fbef98105db24b33
DevonMartens <dmmartens91@gmail.com>
2020-02-03 15:27:28 -0500

    commiting before changing api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
+const express = require('express');
 const userRoutes = require("./user");
 const questionRoutes = require("./questions");
-
+const bcrypt = require('bcryptjs');
+let Registration = require('./schema/User');
+let RouteNames = require("./constants/constants");
+//NOTE  Registration route
 // User routes
 router.use("/user", userRoutes);
 //Questions routes
