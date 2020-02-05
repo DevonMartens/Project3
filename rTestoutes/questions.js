1984fe563996b647d9cb66dfb659c617fe6176c5
Andro Khangulyan <androkhan@gmail.com>
2020-02-04 20:17:10 -0500

    RouteTests

---
+++ rTestoutes/questions.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const questionController = require("../../controllers/questionController");
+
+
+router.route("/")
+  .get(questionController.findAll)
+  .post(questionController.create);
+
+router
+  .route("/:id")
+  .get(questionController.findById)
+  .put(questionController.update)
+  .delete(questionController.remove);
+
+module.exports = router;
